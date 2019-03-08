0787d3a989ccf4b0bd50dddc470b828b4225d5f5
SrikantTTN <srikant.karnani@tothenew.com>
2019-03-08 13:39:16 +0530

    Handle Notification

--- app/src/main/java/com/example/firebase2/FirebaseMessagingService.kt
+++ app/src/main/java/com/example/firebase2/FirebaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.example.firebase2
 
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -15,21 +16,30 @@ class FirebaseMessagingService : FirebaseMessagingService() {
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "Message data payload: " + remoteMessage.data)
-
-            handleFCMessageNow()
-
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+
+            handleFCMessageNow(
+                remoteMessage.notification!!.title,
+                remoteMessage.notification!!.body,
+                remoteMessage.notification!!.channelId
+            )
+
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    private fun handleFCMessageNow() {
-
+    private fun handleFCMessageNow(title: String?, body: String?, channelId: String?) {
+        var builder = NotificationCompat.Builder(this, channelId ?: "1")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        builder.build()
     }
 }
