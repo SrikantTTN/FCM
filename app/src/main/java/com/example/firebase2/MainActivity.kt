2f0298f5d9656659fac24b48e7381a3492a18940
SrikantTTN <srikant.karnani@tothenew.com>
2019-03-08 12:34:39 +0530

    Get and Store Token

--- app/src/main/java/com/example/firebase2/MainActivity.kt
+++ app/src/main/java/com/example/firebase2/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.firebase2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener { instanceIdResult ->
+            run {
+                val token = instanceIdResult.token
+                Log.d("FCM-Token", token)
+                Toast.makeText(this@MainActivity, "Got Token", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
